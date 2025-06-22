8f9c106a842a8bed3ffeea8012da4981c42f1429
Kacper-77 <ks.kontaktowy7@gmail.com>
2025-06-22 15:47:55 +0200

    monorepo init

--- src/main/java/com/domain/todo_app/db/todo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.domain.todo_app.db.todo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
